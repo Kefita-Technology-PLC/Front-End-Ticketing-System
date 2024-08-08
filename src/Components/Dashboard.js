6f68ed0ac490ca0e5fd262f41ca67d774f3f89ff
Biruk Lemma <biruklemmadebela@gmail.com>
2024-08-08 11:02:41 +0300

    dashboard

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -11,6 +11,7 @@ export default function Dashboard() {
     <div className="flex flex-col gap-4">
       <div className="flex flex-row gap-4 w-full"></div>
         <h1>Welcome to Dashboard</h1>
+        
       <div className="flex flex-row gap-4 w-full"></div>
     </div>
   );
